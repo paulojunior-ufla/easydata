df4247a7e48323722f512018d9b58beff0bd0e95
altieresl <altieresl@outlook.com>
2022-05-11 00:56:50 -0300

    fix(geral): remove useless stuff from app component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <navbar-geral/>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
