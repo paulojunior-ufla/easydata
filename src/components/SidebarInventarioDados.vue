aa152c3a7eada331c3c6f7fa09ace9e0c63f41ad
altieresl <altieresl@outlook.com>
2022-05-11 00:55:45 -0300

    feat(sidebar): add sidebar components

---
+++ src/components/SidebarInventarioDados.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" variant="light" class="p-1 py-2 ms-2">
+      <b-nav vertical class="w-100 text-start">
+        <sidebar-inventario-dados-link v-bind:completo="true" texto="Identificação"/>
+        <sidebar-inventario-dados-link v-bind:completo="false" texto="Agentes"/>
+        <sidebar-inventario-dados-link v-bind:completo="true" texto="Ciclo de vida"/>
+        <sidebar-inventario-dados-link v-bind:completo="false" texto="Categorias"/>
+      </b-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import SidebarInventarioDadosLink from './SidebarInventarioDadosLink.vue'
+export default {
+  components: { SidebarInventarioDadosLink },
+  name: 'SidebarInventarioDados',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/components/SidebarInventarioDadosLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-nav-item active>
+      <b-icon style="height: 16px" :icon=" completo ? 'check-lg' : 'x-lg' "/> {{texto}}
+    </b-nav-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarInventarioDadosLink',
+  props: {
+    completo: Boolean,
+    texto: String
+  }
+}
+</script>
