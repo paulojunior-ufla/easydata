b732f5f3784d0bfabb08ec200b9283bf03237e29
altieresl <altieresl@outlook.com>
2022-08-06 21:59:42 -0300

    feat(pdf-page): remove test pdf page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CadastrarInventarioView from '../views/CadastrarInventarioView.vue'
-import PdfInventarioView from '../views/PdfInventarioView.vue'
 import ImportarInventarioView from '../views/ImportarInventarioView.vue'
 
 Vue.use(VueRouter)
@@ -22,11 +21,6 @@ const routes = [
     path: '/editarInventario/:id',
     name: 'editarInventario',
     component: CadastrarInventarioView
-  },
-  {
-    path: '/baixarPdfInventario/:id',
-    name: 'baixarPdfInventario',
-    component: PdfInventarioView
   },
   {
     path: '/importarInventario',
