d3237899d30e6c58414b042ad717d30477846abc
altieresl <altieresl@outlook.com>
2022-08-06 21:25:39 -0300

    fix(router): remove about route

--- src/router/index.js
+++ src/router/index.js
@@ -32,14 +32,6 @@ const routes = [
     path: '/importarInventario',
     name: 'importarInventario',
     component: ImportarInventarioView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
